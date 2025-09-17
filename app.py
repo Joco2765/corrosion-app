43e9a8afd9f639e2869f924ea6801870170bccda
Joco2765 <jonatco289@gmail.com>
2025-09-17 13:06:41 +0800

    app.py

--- app.py
+++ app.py
@@ -230,7 +230,7 @@ with st.sidebar:
     st.header("⚙️ Settings")
     ckpt = st.text_input(
         "Checkpoint (.pth)",
-        value=r"hf://Jonathan78520/corrosion-hrnet-w48/Checkpoint.pth",
+        value="hf://Jonathan78520/corrosion-hrnet-w48/Checkpoint.pth",
         help="Path to your model weights (.pth). On Streamlit Cloud, put the file in your repo and use a relative path like 'checkpoints/model.pth'."
     )
 
@@ -449,6 +449,7 @@ with tab3:
             for p in (in_path, out_path):
                 try: os.remove(p)
                 except Exception: pass
+
 
 
 
